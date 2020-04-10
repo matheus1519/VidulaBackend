ce74576cca23453a48fe09293cec15c033b30ace
matheus1519 <1519academico@gmail.com>
2020-04-10 04:15:10 -0300

    API ajustada

--- src/main/java/com/vidula/model/Assunto.java
+++ src/main/java/com/vidula/model/Assunto.java
@@ -1,11 +1,17 @@
 package com.vidula.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Assunto implements Serializable {
@@ -14,9 +20,31 @@ public class Assunto implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    private String nome;
+
+    @OneToOne
     private Video inicio;
     
-    private int posicao;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JsonBackReference
+    private Disciplina disciplina;
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplica(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public Long getId() {
         return id;
@@ -34,13 +62,4 @@ public class Assunto implements Serializable {
         this.inicio = inicio;
     }
 
-    public int getPosicao() {
-        return posicao;
-    }
-
-    public void setPosicao(int posicao) {
-        this.posicao = posicao;
-    }
-
-    
 }
--- src/main/java/com/vidula/model/Disciplina.java
+++ src/main/java/com/vidula/model/Disciplina.java
@@ -1,11 +1,16 @@
 package com.vidula.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Disciplina implements Serializable {
@@ -17,8 +22,20 @@ public class Disciplina implements Serializable {
     @Column(length = 100, nullable = false)
     private String nome;
     
-    @Column(length = 100, nullable = false)
+    @Column(length = 100, nullable = true)
     private String descricao;
+    
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "disciplina")
+    @JsonManagedReference
+    private List<Assunto> assuntos;
+
+    public List<Assunto> getAssuntos() {
+        return assuntos;
+    }
+
+    public void setAssuntos(List<Assunto> assuntos) {
+        this.assuntos = assuntos;
+    }
 
     public Long getId() {
         return id;
